d5d7b813abebc8490fc1bb55b1eeba24998ca858
Alvaro-SP <socop2412@gmail.com>
2022-03-26 22:40:08 -0600

    Agregando TECLADO inicio add new commit auto

---
+++ commit Compi.bat
@@ -0,0 +1,5 @@
+cd "C:\Users\dell\OneDrive - Facultad de Ingeniería de la Universidad de San Carlos de Guatemala\5 Q U I N T O S E MESTRE\LAB. Orga\PRACTICA 3\-ORGA-P3_G7"
+git add .
+git commit -m "Agregando TECLADO inicio add new commit auto"
+git push
+pause
