8eecb2ab7dc0b06369de8ce2638bedb3b6a9c608
Alvaro-SP <socop2412@gmail.com>
2022-04-06 12:37:21 -0600

    nuevo commit

--- commit Compi.bat
+++ commit Compi.bat
@@ -1,6 +1,6 @@
 cd "C:\Users\SPUser\OneDrive - Facultad de Ingeniería de la Universidad de San Carlos de Guatemala\Documentos\-ORGA-P3_G7"
 @REM cd "C:\Users\dell\OneDrive - Facultad de Ingeniería de la Universidad de San Carlos de Guatemala\5 Q U I N T O S E MESTRE\LAB. Orga\PRACTICA 3\-ORGA-P3_G7"
 git add .
-git commit -m "modificacion de enunciado "
+git commit -m "nuevo commit "
 git push
 pause
