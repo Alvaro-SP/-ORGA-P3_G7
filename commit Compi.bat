a840d9d6702e7da7ccf3d5bf69b80f6c5e6716bb
Alvaro-SP <socop2412@gmail.com>
2022-04-06 12:34:42 -0600

    modificacion de enunciado

--- commit Compi.bat
+++ commit Compi.bat
@@ -1,4 +1,5 @@
-cd "C:\Users\dell\OneDrive - Facultad de Ingeniería de la Universidad de San Carlos de Guatemala\5 Q U I N T O S E MESTRE\LAB. Orga\PRACTICA 3\-ORGA-P3_G7"
+cd "C:\Users\SPUser\OneDrive - Facultad de Ingeniería de la Universidad de San Carlos de Guatemala\Documentos\-ORGA-P3_G7"
+@REM cd "C:\Users\dell\OneDrive - Facultad de Ingeniería de la Universidad de San Carlos de Guatemala\5 Q U I N T O S E MESTRE\LAB. Orga\PRACTICA 3\-ORGA-P3_G7"
 git add .
 git commit -m "modificacion de enunciado "
 git push
